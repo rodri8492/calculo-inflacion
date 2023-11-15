c76d04fc5ba80dff8c1fe89fe61f039af6e37434
rodri8492 <bravodeltamike69@gmail.com>
2023-11-15 16:21:03 -0300

    arreglos importantes

--- index.js
+++ index.js
@@ -44,25 +44,31 @@ class ProductoCarrefour{
 
 //-----------------creando los objetos----------------
 
+// creando la funcion que calcule la inflación
+const calculoInflacion = (precioInicial, precioFinal) =>{
+    let resultado = parseFloat((precioFinal - precioInicial) / precioInicial * 100);
+    let resultadoRedondeado = resultado.toFixed(2);
+    return resultadoRedondeado
+};
 // LECHE
-const lecheDia = new ProductoDia(506, 412, null, null);
-const lecheCarrefour = new ProductoCarrefour(533, 413, null, null);
+const lecheDia = new ProductoDia(506, 412, 402, calculoInflacion(506, 402));
+const lecheCarrefour = new ProductoCarrefour(533, 413, 407, calculoInflacion(533, 407));
 
 // QUESO CREMA COMUN
-const quesoDia = new ProductoDia(935, 1198, null, null);
-const quesoCarrefour = new ProductoCarrefour(1001, 913, null, null);
+const quesoDia = new ProductoDia(935, 1198, 1467, calculoInflacion(935, 1467));
+const quesoCarrefour = new ProductoCarrefour(1001, 913, 1480, calculoInflacion(1001, 1480));
 
 // MANTECA
-const mantecaDia = new ProductoDia(702, 733, null, null);
-const mantecaCarrefour = new ProductoCarrefour(712, 764, null, null);
+const mantecaDia = new ProductoDia(702, 733, 733, calculoInflacion(702, 733));
+const mantecaCarrefour = new ProductoCarrefour(712, 764, 784, calculoInflacion(712, 784));
 
 // CREMA PARA COCINAR 
-const cremaDia = new ProductoDia(1061, 1070, null, null);
-const cremaCarrefour = new ProductoCarrefour(1070, 1125, null, null);
+const cremaDia = new ProductoDia(1061, 1070, 1281, calculoInflacion(1061, 1281));
+const cremaCarrefour = new ProductoCarrefour(1070, 1125, 1113, calculoInflacion(1070, 1113));
 
 // YOGURT BEBIBLE
-const yogurtDia = new ProductoDia(820, 861, null, null);
-const yogurtCarrefour = new ProductoCarrefour(820, 864, null, null);
+const yogurtDia = new ProductoDia(820, 861, 861, calculoInflacion(820, 861));
+const yogurtCarrefour = new ProductoCarrefour(820, 864, 864, calculoInflacion(820, 864));
 
 // -----Creando funciones-----------
 
@@ -71,26 +77,63 @@ const ocultarResultados = () =>{
     tablaBusqueda.style.display = "none";
     graficoBusqueda.style.display = "none";
 };
+
+
 ocultarResultados();
+
+
 const mostrarResultados = () =>{
     resultadoBusqueda.style.display = "block";
     tablaBusqueda.style.display = "grid";
     graficoBusqueda.style.display = "block";
 };
 
+const removerClases = () =>{
+    precioDia1.classList.remove("incremento");
+    precioDia2.classList.remove("incremento");
+    precioDia3.classList.remove("incremento");
+    precioCarrefour1.classList.remove("incremento");
+    precioCarrefour2.classList.remove("incremento");
+    precioCarrefour3.classList.remove("incremento");
+
+    precioDia1.classList.remove("decremento");
+    precioDia2.classList.remove("decremento");
+    precioDia3.classList.remove("decremento");
+    precioCarrefour1.classList.remove("decremento");
+    precioCarrefour2.classList.remove("decremento");
+    precioCarrefour3.classList.remove("decremento");
+
+    precioDia1.classList.remove("equilibrio");
+    precioDia2.classList.remove("equilibrio");
+    precioDia3.classList.remove("equilibrio");
+    precioCarrefour1.classList.remove("equilibrio");
+    precioCarrefour2.classList.remove("equilibrio");
+    precioCarrefour3.classList.remove("equilibrio");
+
+
+    incrementoDia.classList.remove("incremento");
+    incrementoCarrefour.classList.remove("decremento");
+};
+
 const corroborarLeche = ()=>{
+
+    removerClases();
     // DIA
 
     if (lecheDia.precio2 > lecheDia.precio1){
         precioDia2.classList.add("incremento");
     } else if (lecheDia.precio2 < lecheDia.precio1){
         precioDia2.classList.add("decremento");
+    }else if (lecheDia.precio2 == lecheDia.precio1){
+        precioDia2.classList.add("equilibrio");
     }
 
     if (lecheDia.precio3 > lecheDia.precio2){
         precioDia3.classList.add("incremento");
     } else if (lecheDia.precio3 < lecheDia.precio2){
         precioDia3.classList.add("decremento");
+    }else if (lecheDia.precio3 == lecheDia.precio2){
+        precioDia3.classList.add("equilibrio");
     }
 
     if (lecheDia.incremento > 0){
@@ -104,12 +147,16 @@ const corroborarLeche = ()=>{
         precioCarrefour2.classList.add("incremento");
     } else if (lecheCarrefour.precio2 < lecheCarrefour.precio1){
         precioCarrefour2.classList.add("decremento");
+    }else if (lecheCarrefour.precio2 == lecheCarrefour.precio1){
+        precioCarrefour2.classList.add("equilibrio");
     }
 
     if (lecheCarrefour.precio3 > lecheCarrefour.precio2){
         precioCarrefour3.classList.add("incremento");
     } else if (lecheCarrefour.precio3 < lecheCarrefour.precio2){
         precioCarrefour3.classList.add("decremento");
+    }else if (lecheCarrefour.precio3 == lecheCarrefour.precio2){
+        precioCarrefour3.classList.add("equilibrio");
     }
 
     if (lecheCarrefour.incremento > 0){
@@ -117,21 +164,27 @@ const corroborarLeche = ()=>{
     } else if (lecheCarrefour.incremento < 0){
         incrementoCarrefour.classList.add("decremento");
     }
-};
+}
 
 const corroborarYogurt = ()=>{
+
+    removerClases();
     // DIA
 
     if (yogurtDia.precio2 > yogurtDia.precio1){
         precioDia2.classList.add("incremento");
     } else if (yogurtDia.precio2 < yogurtDia.precio1){
         precioDia2.classList.add("decremento");
+    }else if (yogurtDia.precio2 == yogurtDia.precio1){
+        precioDia2.classList.add("equilibrio");
     }
 
     if (yogurtDia.precio3 > yogurtDia.precio2){
         precioDia3.classList.add("incremento");
     } else if (yogurtDia.precio3 < yogurtDia.precio2){
         precioDia3.classList.add("decremento");
+    }else if (yogurtDia.precio3 == yogurtDia.precio2){
+        precioDia3.classList.add("equilibrio");
     }
 
     if (yogurtDia.incremento > 0){
@@ -145,12 +198,16 @@ const corroborarYogurt = ()=>{
         precioCarrefour2.classList.add("incremento");
     } else if (yogurtCarrefour.precio2 < yogurtCarrefour.precio1){
         precioCarrefour2.classList.add("decremento");
+    }else if (yogurtCarrefour.precio2 == yogurtCarrefour.precio1){
+        precioCarrefour2.classList.add("equilibrio");
     }
 
     if (yogurtCarrefour.precio3 > yogurtCarrefour.precio2){
         precioCarrefour3.classList.add("incremento");
     } else if (yogurtCarrefour.precio3 < yogurtCarrefour.precio2){
         precioCarrefour3.classList.add("decremento");
+    }else if (yogurtCarrefour.precio3 == yogurtCarrefour.precio2){
+        precioCarrefour3.classList.add("equilibrio");
     }
 
     if (yogurtCarrefour.incremento > 0){
@@ -158,123 +215,153 @@ const corroborarYogurt = ()=>{
     } else if (yogurtCarrefour.incremento < 0){
         incrementoCarrefour.classList.add("decremento");
     }
-};
+}
 
 const corroborarManteca = ()=>{
+
+    removerClases();
     // DIA
 
     if (mantecaDia.precio2 > mantecaDia.precio1){
         precioDia2.classList.add("incremento");
-    } else if (mantecaDia.precio2 < mantecaDia.precio1){
+    }else if (mantecaDia.precio2 < mantecaDia.precio1){
         precioDia2.classList.add("decremento");
+    }else if (mantecaDia.precio2 == mantecaDia.precio1){
+        precioDia2.classList.add("equilibrio");
     }
 
     if (mantecaDia.precio3 > mantecaDia.precio2){
         precioDia3.classList.add("incremento");
-    } else if (mantecaDia.precio3 < mantecaDia.precio2){
+    }else if (mantecaDia.precio3 < mantecaDia.precio2){
         precioDia3.classList.add("decremento");
+    }else if (mantecaDia.precio3 == mantecaDia.precio2){
+        precioDia3.classList.add("equilibrio");
     }
 
     if (mantecaDia.incremento > 0){
         incrementoDia.classList.add("incremento");
-    } else if (mantecaDia.incremento < 0){
+    }else if (mantecaDia.incremento < 0){
         incrementoDia.classList.add("decremento");
     }
 
     // CARREFOUR
     if (mantecaCarrefour.precio2 > mantecaCarrefour.precio1){
         precioCarrefour2.classList.add("incremento");
-    } else if (mantecaCarrefour.precio2 < mantecaCarrefour.precio1){
+    }else if (mantecaCarrefour.precio2 < mantecaCarrefour.precio1){
         precioCarrefour2.classList.add("decremento");
+    }else if (mantecaCarrefour.precio2 == mantecaCarrefour.precio1){
+        precioCarrefour2.classList.add("equilibrio");
     }
 
     if (mantecaCarrefour.precio3 > mantecaCarrefour.precio2){
         precioCarrefour3.classList.add("incremento");
-    } else if (mantecaCarrefour.precio3 < mantecaCarrefour.precio2){
+    }else if (mantecaCarrefour.precio3 < mantecaCarrefour.precio2){
         precioCarrefour3.classList.add("decremento");
+    }else if (mantecaCarrefour.precio3 == mantecaCarrefour.precio2){
+        precioCarrefour3.classList.add("equilibrio");
     }
 
     if (mantecaCarrefour.incremento > 0){
         incrementoCarrefour.classList.add("incremento");
-    } else if (mantecaCarrefour.incremento < 0){
+    }else if (mantecaCarrefour.incremento < 0){
         incrementoCarrefour.classList.add("decremento");
     }
 }
 
 const corroborarQueso = ()=>{
+
+    removerClases();
     // DIA
 
     if (quesoDia.precio2 > quesoDia.precio1){
         precioDia2.classList.add("incremento");
-    } else if (quesoDia.precio2 < quesoDia.precio1){
+    }else if (quesoDia.precio2 < quesoDia.precio1){
         precioDia2.classList.add("decremento");
+    }else if (quesoDia.precio2 == quesoDia.precio1){
+        precioDia2.classList.add("equilibrio");
     }
 
     if (quesoDia.precio3 > quesoDia.precio2){
         precioDia3.classList.add("incremento");
-    } else if (quesoDia.precio3 < quesoDia.precio2){
+    }else if (quesoDia.precio3 < quesoDia.precio2){
         precioDia3.classList.add("decremento");
+    }else if (quesoDia.precio3 == quesoDia.precio2){
+        precioDia3.classList.add("equilibrio");
     }
 
     if (quesoDia.incremento > 0){
         incrementoDia.classList.add("incremento");
-    } else if (quesoDia.incremento < 0){
+    }else if (quesoDia.incremento < 0){
         incrementoDia.classList.add("decremento");
     }
 
     // CARREFOUR
     if (quesoCarrefour.precio2 > quesoCarrefour.precio1){
         precioCarrefour2.classList.add("incremento");
-    } else if (quesoCarrefour.precio2 < quesoCarrefour.precio1){
+    }else if (quesoCarrefour.precio2 < quesoCarrefour.precio1){
         precioCarrefour2.classList.add("decremento");
+    }else  if (quesoCarrefour.precio2 == quesoCarrefour.precio1){
+        precioCarrefour2.classList.add("equilibrio");
     }
 
     if (quesoCarrefour.precio3 > quesoCarrefour.precio2){
         precioCarrefour3.classList.add("incremento");
-    } else if (quesoCarrefour.precio3 < quesoCarrefour.precio2){
+    }else if (quesoCarrefour.precio3 < quesoCarrefour.precio2){
         precioCarrefour3.classList.add("decremento");
+    }else if (quesoCarrefour.precio3 == quesoCarrefour.precio2){
+        precioCarrefour3.classList.add("equilibrio");
     }
 
     if (quesoCarrefour.incremento > 0){
         incrementoCarrefour.classList.add("incremento");
-    } else if (quesoCarrefour.incremento < 0){
+    }else if (quesoCarrefour.incremento < 0){
         incrementoCarrefour.classList.add("decremento");
     }
-};
+}
 
 const corroborarCrema= ()=>{
+
+    removerClases();
     // DIA
 
     if (cremaDia.precio2 > cremaDia.precio1){
         precioDia2.classList.add("incremento");
-    } else if (cremaDia.precio2 < cremaDia.precio1){
+    }else if (cremaDia.precio2 < cremaDia.precio1){
         precioDia2.classList.add("decremento");
+    }else if (cremaDia.precio2 == cremaDia.precio1){
+        precioDia2.classList.add("equilibrio");
     }
 
     if (cremaDia.precio3 > cremaDia.precio2){
         precioDia3.classList.add("incremento");
-    } else if (cremaDia.precio3 < cremaDia.precio2){
+    }else if (cremaDia.precio3 < cremaDia.precio2){
         precioDia3.classList.add("decremento");
+    }else if (cremaDia.precio3 == cremaDia.precio2){
+        precioDia3.classList.add("equilibrio");
     }
 
     if (cremaDia.incremento > 0){
         incrementoDia.classList.add("incremento");
-    } else if (cremaDia.incremento < 0){
+    }else if (cremaDia.incremento < 0){
         incrementoDia.classList.add("decremento");
     }
 
     // CARREFOUR
     if (cremaCarrefour.precio2 > cremaCarrefour.precio1){
         precioCarrefour2.classList.add("incremento");
-    } else if (cremaCarrefour.precio2 < cremaCarrefour.precio1){
+    }else if (cremaCarrefour.precio2 < cremaCarrefour.precio1){
         precioCarrefour2.classList.add("decremento");
-    }
+    }else if (cremaCarrefour.precio2 == cremaCarrefour.precio1){
+        precioCarrefour2.classList.add("equilibrio");
+    };
 
     if (cremaCarrefour.precio3 > cremaCarrefour.precio2){
         precioCarrefour3.classList.add("incremento");
-    } else if (cremaCarrefour.precio3 < cremaCarrefour.precio2){
+    }else if (cremaCarrefour.precio3 < cremaCarrefour.precio2){
         precioCarrefour3.classList.add("decremento");
-    }
+    }else if (cremaCarrefour.precio3 == cremaCarrefour.precio2){
+        precioCarrefour3.classList.add("equilibrio");
+    };
 
     if (cremaCarrefour.incremento > 0){
         incrementoCarrefour.classList.add("incremento");
@@ -395,5 +482,5 @@ botonBuscar.addEventListener("click", ()=>{
             corroborarCrema();
 
         }
-    };
+    }
 });
